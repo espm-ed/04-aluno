8a54f16306fea1451b3b57c516d0e9102bb29604
Antonio Marcos Selmini <marcos.selmini@uol.com.br>
2025-02-28 10:00:20 -0300

    projeto completo

---
+++ src/aluno/Main.java
@@ -0,0 +1,7 @@
+package aluno;
+
+public class Main {
+    public static void main(String[] args) {
+        Util.menu();
+    }
+}
---
+++ src/aluno/Util.java
@@ -0,0 +1,44 @@
+package aluno;
+
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Util {
+    
+    private static List<Aluno> listaAluno = new ArrayList<>();
+
+    public static void menu() {
+        String aux = "Escolha opção\n";
+        aux += "1. Cadastrar aluno\n";
+            aux += "2. Pesquisar aluno\n";
+            aux += "3. Imprimir aluno\n";
+            aux += "4. Finalizar";
+        int opcao = 0;
+
+        do {            
+            opcao = Integer.parseInt(showInputDialog(null, aux));
+            if(opcao < 1 || opcao > 4) {
+                showMessageDialog(null, "Opção inválida");
+            }
+            else {
+                switch(opcao) {
+                    case 1:
+                        cadastrar();
+                        break;
+                }
+            }
+        } while(opcao != 4);
+
+    }
+
+    // método para cadastrar um aluno
+    private static void cadastrar() {
+        int ra = parseInt(showInputDialog("RA"));
+        String nome = showInputDialog("Nome");
+        listaAluno.add(new Aluno(ra, nome));
+    }
+
+}
